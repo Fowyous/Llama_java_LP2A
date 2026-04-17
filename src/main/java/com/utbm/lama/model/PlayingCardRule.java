b2389bc12add92b6ce192a03a45c101efedcc7ba
Fowyous <mohamed.koubaa@utbm.fr>
2026-04-17 17:54:27 +0200

    added a rule but the rule engine is not yet done

---
+++ src/main/java/com/utbm/lama/model/PlayingCardRule.java
@@ -0,0 +1,38 @@
+/**
+ * this is the rule where we determine what card the player is allowed to play depending on the card on
+ * top of the discard pile. For example if the discard pile has a one the player can only play a two.
+ */
+
+public class PlayingCardRule implements Rule {
+
+	//this map contains on the left the card type of the top card and on the right the card type of the card that we can play and doesn't violate the rules of the game.
+	private static final Map<CardType, CardType> VALID_NEXT_CARDS = Map.ofEntries(
+			Map.entry(CardType.ONE, CardType.TWO),    
+			Map.entry(CardType.TWO, CardType.THREE),   
+			Map.entry(CardType.THREE, CardType.FOUR),    
+			Map.entry(CardType.FOUR, CardType.FIVE),   
+			Map.entry(CardType.FIVE, CardType.SIX),    
+			Map.entry(CardType.SIX, CardType.LLAMA),    
+			Map.entry(CardType.LLAMA, CardType.ONE));
+	/**
+	 * @param move the move that is going to be played.
+	 * @param game the current game.
+	 * @return true if the card played is one number above the one on top of the draw pile. If the card on top of the draw pile is a Llama we return true if the card played is a one.
+	 */
+	public boolean isApplicable(Move move, Game game){
+		
+		if(move.MoveType != PLAY_CARD){// if the move is not playing a card then it doesn't consern this rule.
+			return false;
+		}
+
+		CardType topCard = game.discardPile.Peek();
+
+		if (topCard == null) {    
+			throw new IllegalStateException("Discard pile is empty");
+		}
+			// we verify wether the card on top of the pile and the card in the move are applicable.
+		return move.card == VALID_NEXT_CARDS.get(topCard);
+
+
+	}
+}
